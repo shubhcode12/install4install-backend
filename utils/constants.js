178f08167c0c9c385417269fb606256117b39ca6
shubhcode <shubhcode.dev@gmail.com>
2024-05-17 00:48:08 +0530

    added app constants

---
+++ utils/constants.js
@@ -0,0 +1,13 @@
+const CONSTANT = {
+  valuesForSpinner: [1, 5, 4, 2, 7, 10],
+  valuesForTarget: [
+    1, 1, 1, 1, 1, 1, 1, 1, 5, 5, 4, 4, 4, 2, 2, 2, 2, 2, 2, 7, 10,
+  ],
+  dailyCoinReward: 5,
+  dailySpinReward: 3,
+  bonusSignupCoins: 10,
+  appPromotionCost: 5,
+  appInstallRewardCoins: 5,
+};
+
+module.exports[CONSTANT];
