2d4eec2a2dd1bba440718b026f5ce960c7308fb8
shubhcode <shubhcode.dev@gmail.com>
2024-05-17 13:48:21 +0530

    added constants

--- utils/constants.js
+++ utils/constants.js
@@ -1,13 +1,19 @@
-const CONSTANT = {
-  valuesForSpinner: [1, 5, 4, 2, 7, 10],
-  valuesForTarget: [
-    1, 1, 1, 1, 1, 1, 1, 1, 5, 5, 4, 4, 4, 2, 2, 2, 2, 2, 2, 7, 10,
-  ],
-  dailyCoinReward: 5,
-  dailySpinReward: 3,
-  bonusSignupCoins: 10,
-  appPromotionCost: 5,
-  appInstallRewardCoins: 5,
-};
+// Define each constant
+const valuesForSpinner = [1, 5, 4, 2, 7, 10];
+const valuesForTarget = [1, 1, 1, 1, 1, 1, 1, 1, 5, 5, 4, 4, 4, 2, 2, 2, 2, 2, 2, 7, 10];
+const dailyCoinReward = 5;
+const dailySpinReward = 3;
+const bonusSignupCoins = 10;
+const appPromotionCost = 5;
+const appInstallRewardCoins = 5;
 
-module.exports[CONSTANT];
+// Export each constant individually
+module.exports = {
+  valuesForSpinner,
+  valuesForTarget,
+  dailyCoinReward,
+  dailySpinReward,
+  bonusSignupCoins,
+  appPromotionCost,
+  appInstallRewardCoins,
+};
