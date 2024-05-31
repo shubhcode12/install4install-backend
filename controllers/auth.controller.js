d13128cf4604d7dc8e0ad455fadd3f6fe26beaac
shubhcode <shubhcode.dev@gmail.com>
2024-06-01 01:08:31 +0530

    fix firebase auth

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,22 +1,18 @@
 const { OAuth2Client } = require("google-auth-library");
 const User = require("../models/user.schema");
+const admin = require("firebase-admin");
 
-const CLIENT_ID = process.env.GOOGLE_AUTH_CLIENT_ID;
-const client = new OAuth2Client(CLIENT_ID);
+const initialCoins = 10;
 
 const verifyToken = async (req, res, next) => {
   try {
     const authToken = req.headers.authorization.split("Bearer ")[1];
-    const ticket = await client.verifyIdToken({
-      idToken: authToken,
-      audience: CLIENT_ID,
-    });
-    const payload = ticket.getPayload();
-    req.userData = payload;
-    console.log(payload);
+    const decodedToken = await admin.auth().verifyIdToken(authToken);
+    req.userData = decodedToken;
+    console.log(decodedToken);
     next();
   } catch (error) {
-    res.status(401).json({ error: "Error verifying token" });
+    res.status(401).json({ error: error.message });
   }
 };
 
@@ -31,13 +27,15 @@ const authUser = async (req, res) => {
     } else {
       console.log("User not found, creating new user");
       const newUser = new User({ uid: sub, name: name, email: email });
+      newUser.coins = initialCoins;
+
       await newUser.save();
 
       res.json({ message: "new user created", user: newUser });
     }
   } catch (error) {
     console.error("Error fetching users:", error);
-    res.status(500).json({ "Error fetching users": error.message });
+    res.status(500).json({ error: error.message, message : "auth error" });
   }
 };
 
